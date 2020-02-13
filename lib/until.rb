fc307de809578180283791bc6aca9d9d90bb0326
Yamin Ghowrwal <yamin.gh@gmail.com>
2020-02-13 03:13:06 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,9 +1,8 @@
-
-levitation_force = 0
 def using_until
+  levitation_force = 0
   until levitation_force == 10
-    levitation_force += 1
     puts "Wingardium Leviosa"
+    levitation_force += 1
   end
 end
 
